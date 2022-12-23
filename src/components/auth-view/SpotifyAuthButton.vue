b119df24e51fe99883d4863cb3bfb993caac9724
s.claes <sebastian.claes@protonmail.com>
2022-12-23 18:06:20 +0100

    [ADD] outsourced spotify auth button into own component

---
+++ src/components/auth-view/SpotifyAuthButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SpotifyIcon from "@/components/icons/SpotifyIcon.vue";
+import { SpotifyColorEnum } from "@/models/enums/SpotifyColorEnum.ts";
+</script>
+
+<template>
+  <button class="spotify-oauth2-btn">
+    <SpotifyIcon :color="SpotifyColorEnum.WHITE" />
+    <span>Connect with Spotify</span>
+  </button>
+</template>
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import SpotifyIcon from "@/components/icons/SpotifyIcon.vue";
-import { SpotifyColorEnum } from "@/models/enums/SpotifyColorEnum.ts";
+import SpotifyAuthButton from "@/components/auth-view/SpotifyAuthButton.vue";
+
+function spotifyAuthorization() {
+
+}
 </script>
 
 <template>
@@ -8,10 +11,7 @@ import { SpotifyColorEnum } from "@/models/enums/SpotifyColorEnum.ts";
     <div class="container">
       <h1>CONNECT WITH SPOTIFY</h1>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
-      <button class="spotify-oauth2-btn">
-        <SpotifyIcon :color="SpotifyColorEnum.WHITE" />
-        <span>Connect with Spotify</span>
-      </button>
+      <SpotifyAuthButton @click="spotifyAuthorization" />
     </div>
   </section>
 </template>
