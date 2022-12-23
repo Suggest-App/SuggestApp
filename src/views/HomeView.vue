4dca9c601772957d606fadd908318e0534d1aeec
s.claes <sebastian.claes@protonmail.com>
2022-12-23 17:14:41 +0100

    [ADD] first work session

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="box"></div>
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
+  /**
+   * NOTE: All scss files that contains variables, need to be imported globally and separately!
+   * Globally because otherwise vite can't resolve the variables
+   * Separately because most of the theme scss files that actually contain variables, rely on another
+   */
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/scss/base.scss";
+        `,
+      },
+    },
+  },
 })
