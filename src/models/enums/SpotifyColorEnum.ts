f0e572de09a6dd38e40db3c76f26751f60715284
s.claes <sebastian.claes@protonmail.com>
2022-12-23 17:57:34 +0100

    [ADD] created AuthView.vue background and button styles

---
+++ src/models/enums/SpotifyColorEnum.ts
@@ -0,0 +1,4 @@
+export enum SpotifyColorEnum {
+    WHITE = '#FFFFFF',
+    GREEN = '#1DB954'
+}
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,10 +1,18 @@
-<script setup>
-
+<script setup lang="ts">
+import SpotifyIcon from "@/components/icons/SpotifyIcon.vue";
+import { SpotifyColorEnum } from "@/models/enums/SpotifyColorEnum.ts";
 </script>
 
 <template>
   <section id="auth-view">
-
+    <div class="container">
+      <h1>CONNECT WITH SPOTIFY</h1>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+      <button class="spotify-oauth2-btn">
+        <SpotifyIcon :color="SpotifyColorEnum.WHITE" />
+        <span>Connect with Spotify</span>
+      </button>
+    </div>
   </section>
 </template>
 
