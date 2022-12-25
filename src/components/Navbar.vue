76d94944d958e408f84f96c9a58958314ab23e7a
s.claes <sebastian.claes@protonmail.com>
2022-12-25 12:09:27 +0100

    [NEW] Navbar.vue component and navbar icon components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import HomeViewIcon from "@/components/icons/navbar/HomeViewIcon.vue";
+import RankingViewIcon from "@/components/icons/navbar/RankingViewIcon.vue";
+import ProfileViewIcon from "@/components/icons/navbar/ProfileViewIcon.vue";
+</script>
+
+<template>
+  <nav id="navbar">
+    <RouterLink :to="{ name: 'HomeView' }">
+      <HomeViewIcon  color="#FFFFFF" />
+      <span></span>
+    </RouterLink>
+    <RouterLink :to="{ name: 'RankingView' }">
+      <RankingViewIcon color="#FFFFFF" />
+      <span></span>
+    </RouterLink>
+    <RouterLink :to="{ name: 'ProfileView' }">
+      <ProfileViewIcon color="#FFFFFF" />
+      <span></span>
+    </RouterLink>
+  </nav>
+</template>
+
+
+
+<style>
+@import "@/assets/scss/navbar.scss";
+</style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/profile-view/profile-view.scss";
+</style>
