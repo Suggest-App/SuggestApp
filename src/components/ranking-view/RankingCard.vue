835e5b4da8a9067f42a9f19688d504e2dc82e5f7
s.claes <sebastian.claes@protonmail.com>
2022-12-27 11:03:56 +0100

    [ADD] basic ranking view template with base styles

---
+++ src/components/ranking-view/RankingCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="ranking-card">
+    <div class="rank">1</div>
+    <div class="profile-image"></div>
+    <div class="profile-info-wrapper">
+      <p class="profile-name">Benutzername</p>
+      <h3 class="shared-minutes">2.376 Minuten</h3>
+    </div>
+  </div>
+</template>
--- src/views/RankingView.vue
+++ src/views/RankingView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from "@/components/Navbar.vue";
+import RankingCard from "@/components/ranking-view/RankingCard.vue";
 </script>
 
 <template>
   <section id="ranking-view">
+    <RankingCard v-for="card in 12" :key="card" />
     <Navbar />
   </section>
 </template>
