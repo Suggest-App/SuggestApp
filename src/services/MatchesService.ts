4c6207ab01593276a2efa0fda6cdecc80db4cc0c
s.claes <sebastian.claes@protonmail.com>
2023-01-03 17:14:37 +0100

    [NEW] created MatchesService.ts and MatchesStore.ts

---
+++ src/services/MatchesService.ts
@@ -0,0 +1,38 @@
+import type {Match} from "@/models/Match";
+import {tryGetAuthorizedInstance} from "@/services/TokenService";
+import type {AxiosResponse} from "axios";
+import type {MediaSummary} from "@/models/MediaSummary";
+
+export default {
+    async fetchMatches(): Promise<Match[]>{
+        return tryGetAuthorizedInstance().get('user/matches')
+            .then((response: AxiosResponse) => {
+                return response.data
+            })
+            .catch((error) => {
+                switch (error.response.status) {
+                    default:
+                        console.log(
+                            'MatchesService.ts no status case ' + error.response.status
+                        )
+                        break
+                }
+            })
+    },
+    async fetchRecommendedMedia(): Promise<MediaSummary> {
+        return tryGetAuthorizedInstance().get('user/match/{userId}/recommended-media')
+            .then((response: AxiosResponse) => {
+                return response.data
+            })
+            .catch((error) => {
+                switch (error.response.status) {
+                    default:
+                        console.log(
+                            'MatchesService.ts no status case ' + error.response.status
+                        )
+                        break
+                }
+            })
+    }
+
+}
---
+++ src/stores/MatchesStore.ts
@@ -0,0 +1,11 @@
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { defineStore } from 'pinia'
+import type {Match} from "@/models/Match";
+
+
+export const useMatchesStore = defineStore('matchesStore', () => {
+
+  const matches: Ref<Match[]> = ref([])
+  return { matches }
+})
