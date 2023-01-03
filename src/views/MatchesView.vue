94cea7b5dcd920fc68a41efebc6a00cd1955a502
s.claes <sebastian.claes@protonmail.com>
2023-01-03 17:15:57 +0100

    [ADD] open RecommendedMediaView.vue component on Match click

--- src/views/MatchesView.vue
+++ src/views/MatchesView.vue
@@ -2,12 +2,28 @@
 import Navbar from "@/components/Navbar.vue";
 import Match from "@/components/matches-view/Match.vue";
 import InfoIcon from "@/components/icons/controls/InfoIcon.vue";
+import {onMounted} from "vue";
+import MatchesService from "@/services/MatchesService";
+import {useMatchesStore} from "@/stores/MatchesStore";
+import router from "@/router";
+
+const matchesStore = useMatchesStore()
+
+onMounted(async () => {
+   matchesStore.matches = await MatchesService.fetchMatches()
+})
 </script>
 
 <template>
   <section id="matches-view">
     <h2>Your top matches <InfoIcon /></h2>
-    <Match v-for="index in 15" :key="index" />
+    <Match
+        @click="router.push('/recommended-media')"
+        v-for="(match,index) in matchesStore.matches"
+        :key="match.userId"
+        :match="match"
+        :index="index"
+    />
     <Navbar />
    </section>
  </template>
