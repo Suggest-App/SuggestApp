583bd80088c1fe14860a14fd7ae1f272304c689d
s.claes <sebastian.claes@protonmail.com>
2023-01-03 17:15:14 +0100

    [ADD] bind match data to template

--- src/components/matches-view/Match.vue
+++ src/components/matches-view/Match.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
-import { PropType } from "vue";
+import {computed, ComputedRef, PropType} from "vue";
 import { Match } from "@/models/Match";
+import {secondsToMinutes} from "@/composables/TimeCalculations";
 
 const props = defineProps({
   match: {
     type: Object as PropType<Match>,
-    required: false,
+    required: true,
   },
+  index: {
+    type: Number,
+    required: true
+  }
+})
+console.log(props.match)
+const minutes:ComputedRef<string> = computed(() => {
+  return props.match
+      ? secondsToMinutes(props.match?.listenedTogetherSeconds)
+      : ""
 })
 </script>
 
 <template>
   <div class="match-container">
-    <div class="rank">1</div>
-    <div class="profile-image"></div>
+    <div class="rank">{{props.index+1}}</div>
+    <img
+        class="profile-image"
+        :src="props.match.profileImage"
+        alt="Profile image"
+    />
     <div class="profile-information">
-      <p class="profile-name">Sebastian Claes</p>
-      <p class="listened-together">10.342 minutes listened together</p>
+      <p class="profile-name">{{props.match.username}}</p>
+      <p class="listened-together">{{minutes}} minutes listened together</p>
     </div>
   </div>
 </template>
