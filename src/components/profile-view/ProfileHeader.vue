771ca0e285137b4fdb1075d89603ed0d62d13aa5
s.claes <sebastian.claes@protonmail.com>
2023-01-04 11:37:34 +0100

    [ADD] outsourced time calculation for the trackingSince date

--- src/components/profile-view/ProfileHeader.vue
+++ src/components/profile-view/ProfileHeader.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import InfoIcon from "@/components/icons/controls/InfoIcon.vue";
-import { computed } from "vue";
-import type { PropType } from "vue";
+import { ref, computed } from "vue";
+import type { Ref, PropType } from "vue";
 import type { ProfileInformation } from "@/models/ProfileInformation";
+import {trackingSinceDate} from "@/composables/TimeCalculations";
 
 const props = defineProps({
   profileInformation: {
@@ -22,22 +23,15 @@ const username = computed(() => {
 })
 
 // value since when user records are getting tracked
+const showTrackingSince: Ref<boolean> = ref(props.profileInformation.trackingSince != null)
+
+// value since when user records are getting tracked
 const trackingSince = computed(() => {
-  // Get date object based on datetime string
-  let date = new Date(props.profileInformation.trackingSince);
-
-  // get single date values and add 0 from german pattern if necessary
-  let day = (date.getDay() < 10) ? '0' + date.getDay() : date.getDay()
-  let month = (date.getMonth() < 10) ? '0' + date.getMonth() : date.getMonth()
-  let year = date.getFullYear()
-
-  return day + '.' + month + '.' + year
+  return (props.profileInformation && showTrackingSince.value)
+    ? trackingSinceDate(props.profileInformation.trackingSince)
+    : ''
 })
 
-// Only display tracking since phrase, if tracking value exists
-const showTrackingSince = computed(() => {
-  return props.profileInformation.trackingSince !== null
-})
 
 // Font size of username should be larger, if no tracking since value is displayed
 const usernameFontSize = computed(() => {
--- src/composables/TimeCalculations.ts
+++ src/composables/TimeCalculations.ts
@@ -1,5 +1,27 @@
-export function secondsToMinutes(seconds:number) : string {
+
+/**
+ * Calculate seconds to minutes and round te result down to the next full minute
+ *
+ * @param seconds number
+ *
+ * @return string
+ */
+export function secondsToMinutes(seconds: number): string {
     let minutes: number = seconds / 60
     let roundedMinutes: number = Math.floor(minutes)
-    return roundedMinutes.toLocaleString("de-DE");
+    return roundedMinutes.toLocaleString('de-DE');
+}
+
+/**
+ * Format UTC date string to german local date
+ *
+ * @param dateString string
+ *
+ * @return string
+ */
+export function trackingSinceDate(dateString: string): string {
+    return new Date(dateString).toLocaleString(
+        'de-DE',
+        { timeZone: 'Europe/Luxembourg' }
+    ).split(',')[0]
 }
