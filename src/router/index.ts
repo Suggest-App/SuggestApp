6177020ce48d917da5bb31b1017231a41f1dfb6c
s.claes <sebastian.claes@protonmail.com>
2023-01-05 11:12:22 +0100

    [ADD] Check before each router view change if the user is authorized

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteLocationNormalized } from 'vue-router'
 import AuthView from "@/views/AuthView.vue";
 import ShuffleView from '@/views/ShuffleView.vue'
 import MatchesView from "@/views/MatchesView.vue";
 import ProfileView from "@/views/ProfileView.vue";
 import RecommendedMediaView from "@/views/RecommendedMediaView.vue";
+import { getCookie } from "@/services/TokenService";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,6 +36,28 @@ const router = createRouter({
       component: RecommendedMediaView
     }
   ]
+})
+
+/**
+ * Check before each router view change, if the user has a valid token
+ * If no redirect him to auth view
+ *
+ * @param toRoute RouteLocationNormalized
+ *
+ * @return Promise<string | void>
+ */
+router.beforeEach(async (toRoute: RouteLocationNormalized): Promise<string | void> => {
+
+  // all pages that doesn't require authorization
+  const publicPages = ['/']
+
+  // Bool that indicates if target route requires a token
+  const tokenRequired = !publicPages.includes(toRoute.path)
+
+  // Redirect if route require token, but there is no token in local storage
+  if (tokenRequired && !getCookie('jwt')) {
+    return '/'
+  }
 })
 
 export default router
