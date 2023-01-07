cb23ca038f829bef80636822b1cd72c642c533e4
s.claes <sebastian.claes@protonmail.com>
2023-01-07 13:49:23 +0100

    [ADD] created matchesMap in orde rto gte natch by its userId

--- src/stores/MatchesStore.ts
+++ src/stores/MatchesStore.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { reactive, ref} from 'vue'
 import type { Ref } from 'vue'
 import { defineStore } from 'pinia'
 import type {Match} from "@/models/Match";
@@ -9,11 +9,16 @@ export const useMatchesStore = defineStore('matchesStore', () => {
   // Array that holds all user matches
   const matches: Ref<Match[]> = ref([])
 
+  // Pool that contains the whole asset data objects
+  const matchesMap: Map<string, Match> = reactive(
+      new Map<string, Match>()
+  )
+
   // Flag that indicates if the matches are loading
   const isLoading: Ref<boolean> = ref(true)
 
   // Array that holds the media from a match that the user don't know
   const recommendedMedia: Ref<MediaSummary[]> = ref([] as MediaSummary[]);
 
-  return { matches, isLoading, recommendedMedia }
+  return { matches, matchesMap, isLoading, recommendedMedia }
 })
