5dae102912e697f8e8693f263ccd01c1419b3892
s.claes <sebastian.claes@protonmail.com>
2023-01-13 12:33:12 +0100

    [ADD] auth view stylings on mobile

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,12 +1,56 @@
 <script setup lang="ts">
+import SuggestLogo from "@/components/icons/SuggestLogo.vue";
 import SpotifyAuthButton from "@/components/auth-view/SpotifyAuthButton.vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, Autoplay } from 'swiper';
+
+// define your modules list here
+const modules = [Pagination, Autoplay]
+
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/pagination";
+
 </script>
 
 <template>
   <section id="auth-view">
-    <form action="/signin" method="post" class="container">
-      <h1>CONNECT WITH SPOTIFY</h1>
-      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+    <div class="image-container">
+      <img src="src/assets/images/auth-view-background.png" alt="Suggest background image" />
+    </div>
+    <form action="/signin" method="post" class="auth-form">
+      <SuggestLogo />
+      <swiper
+          :slides-per-view="1"
+          :modules="modules"
+          :loop="true"
+          :pagination="{
+            clickable: true,
+          }"
+          :autoplay="{
+            delay: 3500,
+            disableOnInteraction: false,
+          }"
+      >
+        <swiper-slide>
+          <h1>Suggest</h1>
+          <p>Find new stunning music you don’t know,
+            based on the taste of users that are
+            the same as you.</p>
+        </swiper-slide>
+        <swiper-slide>
+          <h1>Suggest</h1>
+          <p>Find new stunning music you don’t know,
+            based on the taste of users that are
+            the same as you.</p>
+        </swiper-slide>
+        <swiper-slide>
+          <h1>Suggest</h1>
+          <p>Find new stunning music you don’t know,
+            based on the taste of users that are
+            the same as you.</p>
+        </swiper-slide>
+      </swiper>
       <SpotifyAuthButton />
     </form>
   </section>
