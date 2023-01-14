825e4bda5ff14a82097525f5d79fb5a90ebe6c12
s.claes <sebastian.claes@protonmail.com>
2023-01-14 13:59:06 +0100

    [ADD] desktop auth responsive styles

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -19,39 +19,41 @@ import "swiper/css/pagination";
       <img src="@/../public/images/auth-view-background.png" alt="Suggest Background Image" />
     </div>
     <form action="/signin" method="post" class="auth-form">
-      <SuggestLogo />
-      <swiper
-          :slides-per-view="1"
-          :modules="modules"
-          :loop="true"
-          :pagination="{
-            clickable: true,
-          }"
-          :autoplay="{
-            delay: 3500,
-            disableOnInteraction: false,
-          }"
-      >
-        <swiper-slide>
-          <h1>Suggest</h1>
-          <p>Find new stunning music you don’t know,
-            based on the taste of users that are
-            the same as you.</p>
-        </swiper-slide>
-        <swiper-slide>
-          <h1>Suggest</h1>
-          <p>Find new stunning music you don’t know,
-            based on the taste of users that are
-            the same as you.</p>
-        </swiper-slide>
-        <swiper-slide>
-          <h1>Suggest</h1>
-          <p>Find new stunning music you don’t know,
-            based on the taste of users that are
-            the same as you.</p>
-        </swiper-slide>
-      </swiper>
-      <SpotifyAuthButton />
+      <div class="auth-form-wrapper">
+        <SuggestLogo />
+        <swiper
+            :slides-per-view="1"
+            :modules="modules"
+            :loop="true"
+            :pagination="{
+              clickable: true,
+            }"
+            :autoplay="{
+              delay: 3500,
+              disableOnInteraction: false,
+            }"
+        >
+          <swiper-slide>
+            <h1>Suggest</h1>
+            <p>Find new stunning music you don’t know,
+              based on the taste of users that are
+              the same as you.</p>
+          </swiper-slide>
+          <swiper-slide>
+            <h1>Suggest</h1>
+            <p>Find new stunning music you don’t know,
+              based on the taste of users that are
+              the same as you.</p>
+          </swiper-slide>
+          <swiper-slide>
+            <h1>Suggest</h1>
+            <p>Find new stunning music you don’t know,
+              based on the taste of users that are
+              the same as you.</p>
+          </swiper-slide>
+        </swiper>
+        <SpotifyAuthButton />
+      </div>
     </form>
   </section>
 </template>
