92ab80ae475b4abb7d1a298db028a9b4044a9c4d
s.claes <sebastian.claes@protonmail.com>
2023-04-14 15:00:51 +0200

    [FIX] corrected seconds to minutes formatting method

--- src/composables/TimeCalculations.ts
+++ src/composables/TimeCalculations.ts
@@ -7,22 +7,8 @@
  * @return string
  */
 export function secondsToMinutes(seconds: number): string {
-    let minutes: number = seconds / 60
-    let roundedMinutes: number = Math.round((minutes + Number.EPSILON) * 100) / 100
-    let minutesAsString = roundedMinutes.toLocaleString('de-DE')
-
-    // If there are over 100 Minutes heard, just return the full minutes
-    if (roundedMinutes > 100) {
-        return minutesAsString.split(',')[0]
-    } else {
-
-        let splittetMinutes = minutesAsString.split(',')[1]
-        let formattedMinutes = minutesAsString.replace(',', ':')
-
-        return (splittetMinutes && splittetMinutes.length === 1)
-            ? formattedMinutes + '0'
-            : formattedMinutes
-    }
+    const date = new Date(seconds * 1000).toISOString().substring(14, 19)
+    return date;
 }
 
 /**
