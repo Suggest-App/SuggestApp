1920c406f9a125cbe41e562578b384efa750ea76
s.claes <sebastian.claes@protonmail.com>
2023-04-14 10:40:25 +0200

    [ADD] more precise time calculation

--- src/composables/TimeCalculations.ts
+++ src/composables/TimeCalculations.ts
@@ -8,9 +8,21 @@
  */
 export function secondsToMinutes(seconds: number): string {
     let minutes: number = seconds / 60
-    let roundedMinutes: number = Math.floor(minutes)
-    let formattedMinutes = roundedMinutes.toLocaleString('de-DE');
-    return (formattedMinutes === "0") ? "<1" : formattedMinutes;
+    let roundedMinutes: number = Math.round((minutes + Number.EPSILON) * 100) / 100
+    let minutesAsString = roundedMinutes.toLocaleString('de-DE')
+
+    // If there are over 100 Minutes heard, just return the full minutes
+    if (roundedMinutes > 100) {
+        return minutesAsString.split(',')[0]
+    } else {
+
+        let splittetMinutes = minutesAsString.split(',')[1]
+        let formattedMinutes = minutesAsString.replace(',', ':')
+
+        return (splittetMinutes && splittetMinutes.length === 1)
+            ? formattedMinutes + '0'
+            : formattedMinutes
+    }
 }
 
 /**
