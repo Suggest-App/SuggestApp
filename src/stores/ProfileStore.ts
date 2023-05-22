b95ff98724efc662ac78edafee243bb179b97579
claes-work <s.claes.work@gmail.com>
2023-05-22 16:55:55 +0200

    [ADD] fix

--- src/stores/ProfileStore.ts
+++ src/stores/ProfileStore.ts
@@ -44,7 +44,7 @@ export const useProfileStore = defineStore('profileStore', () => {
       // Set the media array from the /personal-summary endpoint
       user.setMediaSummary(summary)
 
-      await fetchHiddenMedia()
+      //await fetchHiddenMedia()
 
       // Overwrite the profile ref
       profile.value = user
